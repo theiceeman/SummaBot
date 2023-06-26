98b2fc7f4c6a7fa6b89667310649a392392c3826
Chrstphr <somto565@gmail.com>
2023-06-26 11:16:41 +0200

    done with hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
-import { logo } from '../assets'
-
+import React from "react";
+import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <header className='w-full flex justify-center
-    items-center flex-col'>
-        <nav className='flex justify-between items-center flex-col'>
-            <img src={logo} alt='simz_logo' 
-            className='w-28 object-contain'/>
-        </nav>
-
-        <button 
-        type='button'
-        onClick={() => window .open('https://github.com/Chrstphr9/SummaBot2.0.git')}
-        className='black_btn'
+    <header
+      className="w-full flex justify-center
+    items-center flex-col"
+    >
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="simz_logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() =>
+            window.open("https://github.com/Chrstphr9/SummaBot2.0.git")
+          }
+          className="black_btn"
         >
-            GitHub
+          GitHub
         </button>
-    </header>
-  )
-}
+      </nav>
 
-export default Hero
+      <h1 className="head_text">
+        Summarize Article with <br className="max-md-hidden" />
+        <span className="orange_gradient">Open AI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
