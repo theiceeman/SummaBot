1618c1c17fbf5064e85936dd1d136d1063dc36cd
Chrstphr <somto565@gmail.com>
2023-06-26 11:02:47 +0200

    integrating the navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { logo } from '../assets'
 
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className='w-full flex justify-center
+    items-center flex-col'>
+        <nav className='flex justify-between items-center flex-col'>
+            <img src={logo} alt='simz_logo' 
+            className='w-28 object-contain'/>
+        </nav>
+
+        <button 
+        type='button'
+        onClick={() => window .open('https://github.com/Chrstphr9/SummaBot2.0.git')}
+        className='black_btn'
+        >
+            GitHub
+        </button>
+    </header>
   )
 }
 
