06ae3b6e496bb837b7ad32f0d73bb89bfd694645
Chrstphr <somto565@gmail.com>
2023-06-26 11:48:28 +0200

    done with demo ui

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,39 @@
-
+import { useState, useEffect } from 'react';
+import { copy, linkIcon, loader, tick } from '../assets'
+import { MdSend } from 'react-icons/Md';
 
 const Demo = () => {
   return (
-    <div>Demo</div>
+    <section className='mt-16 w-full max-w-xl'>
+         <div className='flex flex-col w-full gap-2'>
+            <form className='relative flex justify-center items-center'
+                onSubmit={() => {}}
+            >
+                <img
+                    src={linkIcon}
+                    alt='link_icon'
+                    className='absolute left-0 my-2 ml-3 w-5'
+                />
+
+                <input 
+                    type='url'
+                    placeholder='Enter A URL'
+                    value=""
+                    onChange={() => {}}
+                    required
+                    className='url_input peer' 
+                />
+
+                <button
+                    type='submit'
+                    className='submot_btn peer-focus:border-gray-700
+                    peer-focus:text-gray-700'
+                > 
+                    <MdSend size="2.5rem"/>
+                </button>
+            </form>
+         </div>
+    </section>
   )
 }
 
