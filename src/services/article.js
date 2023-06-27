f3be87402ca55cafc8639e4c49d79e9df3682db3
Chrstphr <somto565@gmail.com>
2023-06-27 11:52:42 +0200

    included the Api

--- src/services/article.js
+++ src/services/article.js
@@ -1,13 +1,29 @@
-import { createApi, fetchBaseQuery } from 
-"@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
 
+// const axios = require('axios');
+
+const options = {
+  method: "GET",
+  url: "https://article-extractor-and-summarizer.p.rapidapi.com/summarize",
+  params: {
+    url: "https://time.com/6266679/musk-ai-open-letter/",
+    length: "3",
+  },
+  headers: {
+    "X-RapidAPI-Key": "822f1639cdmshaf5f7b4c3550fe8p1fea29jsn0e19333e955e",
+    "X-RapidAPI-Host": "article-extractor-and-summarizer.p.rapidapi.com",
+  },
+};
+
 export const articleApi = createApi({
-    reducerPath: 'articleApi',
-    endpoints: (builder) => ({
-        getSummary: builder.query({
-            query: (params) => `test`
-        })
-    })
-    
+  reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+  }),
+  endpoints: (builder) => ({
+    getSummary: builder.query({
+      query: (params) => `test`,
+    }),
+  }),
 });
