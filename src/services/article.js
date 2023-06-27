d000a9eb4f7a13e03047318f2898a8d7117cdac4
Chrstphr <somto565@gmail.com>
2023-06-27 11:31:17 +0200

    exported store.js

---
+++ src/services/article.js
@@ -0,0 +1,7 @@
+import { createApi, fetchBaseQuery } from 
+"@reduxjs/toolkit/query/react";
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
+
+export const articleApi = createApi({
+    reducerPath: 'articleApi'
+});
